44f55b2a90e8e05da03002d635a4dbaead47cef9
ajscriv <alex.j.scriven@gmail.com>
2018-10-31 14:47:31 +1100

    reservations rendering

--- ba-frontend/src/components/Flight.jsx
+++ ba-frontend/src/components/Flight.jsx
@@ -15,42 +15,55 @@ class Flight extends Component{
 
   componentDidMount(){
     const searchUrl = GET_RESV_URL + this.props.match.params.id
-    console.log("This is the search url", searchUrl);
+    this.performQuery(searchUrl)
   }
 
   performQuery( url ){
     axios.get(url)
     .then( response => {
-      // console.log(response);
-      this.setState({seatAvailability: response.data})
+      console.log(response);
+      this.setState({seatAvailability: response.data.bookings})
     })
     .catch( err => {
       console.warn(err);
     });
   }
 
-  saveReservation(){
-    const reservationParams = {
-      user_id: 1,
-      flight_id: this.props.match.params.id,
-      row: 1,
-      column: 2
-    }
-    axios.post(POST_RESV_URL, {params:reservationParams})
-    .then(response => {
-      this.setState({seatAvailability: response.data})
-      //Please send back the response when post is created
-      // this.setState({secrets: [response.data.secret,...this.state.secrets]})
-    })
-    .catch(console.warn)
-  }
+  // saveReservation(){
+  //   const reservationParams = {
+  //     user_id: 1,
+  //     flight_id: this.props.match.params.id,
+  //     row: 1,
+  //     column: 2
+  //   }
+  //   axios.post(POST_RESV_URL, {params:reservationParams})
+  //   .then(response => {
+  //     this.setState({seatAvailability: response.data})
+  //     //Please send back the response when post is created
+  //     // this.setState({secrets: [response.data.secret,...this.state.secrets]})
+  //   })
+  //   .catch(console.warn)
+  // }
 
   render(){
-    return(
+    return (
       <div>
-      <h1>Pick your seats here!</h1>
+        <br/>
+        <hr/><hr/>
+        <h2>The search results for flight id:  {this.props.match.params.id}!</h2>
+        <ul>
+          {
+            this.state.seatAvailability.map((s,index) =>
+              <ul>
+                <li>Row {index+1} availability: {
+                    s.map(rowCol => <li>{rowCol ? "Yes" : "No"}</li>)
+                  }</li>
+              </ul>
+            )
+          }
+        </ul>
       </div>
-    )
+    );
   }
 }
 
