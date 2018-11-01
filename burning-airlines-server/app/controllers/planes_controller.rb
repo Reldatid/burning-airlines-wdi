aae5b5b789e00e985bdd8e463b3594aabc95d20c
Jonathan Odlum <jonathan.odlum@gmail.com>
2018-11-01 23:05:00 +1100

    not sure what :\

--- burning-airlines-server/app/controllers/planes_controller.rb
+++ burning-airlines-server/app/controllers/planes_controller.rb
@@ -14,9 +14,10 @@ class PlanesController < ApplicationController
 
   def index
     @plane = Plane.new
-    @planes = Plane.all
+    @planes = Plane.all.reverse
     @select_columns = [['1 (A)', 1], ['2 (B)', 2], ['3 (C)', 3], ['4 (D)', 4], ['5 (E)', 5], ['6 (F)', 6], ['7 (G)', 7], ['8 (H)', 8], ['9 (I)', 9], ['10 (J)', 10]]
     @select_rows = (5..50)
+    @columns = ['A','B','C','D','E','F','G','H','I','J']
   end
 
   def destroy
